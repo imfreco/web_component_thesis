ad8ab6e30de251abf3a1bc36b81ee1397ff62df3
fredycorts7 <fred.cor.14@gmail.com>
2020-11-21 23:15:33 -0500

    <ADD> afirmaciones en los casos de prueba actuales y agregando casos de pruebas unitarias para los middlewares del módulo de autenticación restantes

--- src/tests/units/actions/authentication.action.test.js
+++ src/tests/units/actions/authentication.action.test.js
@@ -4,6 +4,8 @@ import '@testing-library/jest-dom';
 import {
   startDictionaryRead,
   startLogIn,
+  startLogOut,
+  startSilentAuthentication,
 } from '../../../actions/authentication.action';
 import { types } from '../../../fixtures/types';
 import * as fetchModule from '../../../helpers/request.helper';
@@ -31,6 +33,7 @@ const create = (currentState) => {
 };
 
 Storage.prototype.setItem = jest.fn();
+Storage.prototype.getItem = jest.fn();
 
 describe('Pruebas unitarias en las acciones de autenticación', () => {
   beforeEach(() => {
@@ -104,6 +107,9 @@ describe('Pruebas unitarias en las acciones de autenticación', () => {
     await invoke(startDictionaryRead());
 
     expect(store.getState).not.toHaveBeenCalled();
+    expect(fetchModule.fetchWithoutToken).toHaveBeenCalledWith(
+      'auth/substitution'
+    );
     expect(jwt.decode).toHaveBeenCalledWith(dict_token);
     expect(dictionaryModule.sortDictionary).toHaveBeenCalledWith(dictionary);
     expect(store.dispatch).toHaveBeenCalledTimes(2);
@@ -113,6 +119,84 @@ describe('Pruebas unitarias en las acciones de autenticación', () => {
     });
     expect(store.dispatch).toHaveBeenCalledWith({
       type: types.uiStopLoading,
+    });
+  });
+
+  test('CP15 - Debería ...', async () => {
+    const refresh_token = 'edm';
+    const new_id_token = 'dto';
+    const new_refresh_token = 'mrt';
+    const userMock = { user: 1, name: 'Fredy', lastname: 'Cortés', roles: [] };
+
+    const { store, invoke } = create({});
+
+    Storage.prototype.getItem = jest.fn(() => refresh_token);
+
+    fetchModule.fetchProtectedResource = jest.fn(() => ({
+      json: () => ({
+        id_token: new_id_token,
+        refresh_token: new_refresh_token,
+      }),
+    }));
+
+    jwt.decode = jest.fn(() => userMock);
+
+    await invoke(startSilentAuthentication());
+
+    expect(store.getState).not.toHaveBeenCalled();
+    expect(localStorage.getItem).toHaveBeenCalledWith(items.refreshToken);
+    expect(fetchModule.fetchProtectedResource).toHaveBeenCalledWith(
+      'auth/refresh',
+      {},
+      'GET',
+      refresh_token
+    );
+    expect(jwt.decode).toHaveBeenCalledWith(new_id_token);
+    expect(localStorage.setItem).toHaveBeenCalledWith(
+      items.refreshToken,
+      new_refresh_token
+    );
+    expect(store.dispatch).toHaveBeenCalledTimes(2);
+    expect(store.dispatch).toHaveBeenCalledWith({
+      type: types.authnLogIn,
+      payload: {
+        id: userMock.user,
+        name: userMock.name,
+        lastname: userMock.lastname,
+        roles: userMock.roles,
+        id_token: new_id_token,
+      },
+    });
+    expect(store.dispatch).toHaveBeenCalledWith({
+      type: types.authnStopLoading,
+    });
+  });
+
+  test('CP16 - Debería ...', async () => {
+    const id = 1;
+
+    const { store, invoke } = create({
+      authenticationReducer: { user: { id } },
+    });
+
+    fetchModule.fetchWithoutToken = jest.fn(() => ({
+      json: () => ({ message: 'ok' }),
+    }));
+
+    Storage.prototype.removeItem = jest.fn();
+
+    await invoke(startLogOut());
+
+    expect(store.getState).toHaveBeenCalledTimes(1);
+    expect(fetchModule.fetchWithoutToken).toHaveBeenCalledWith(
+      `auth/signout/${id}`,
+      {},
+      'PATCH'
+    );
+    expect(localStorage.removeItem).toHaveBeenCalledWith(items.refreshToken);
+    expect(store.dispatch).toHaveBeenCalledTimes(1);
+    expect(store.dispatch).toHaveBeenCalledWith({
+      type: types.authnLogOut,
     });
   });
 });
